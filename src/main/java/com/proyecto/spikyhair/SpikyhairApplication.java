502e48b14b9d2685e547970e49db110e2101f485
danysis.2022@gmail.com <danysis.2022@gmail.com>
2025-05-29 13:04:08 -0500

    Creacion repositorio

---
+++ src/main/java/com/proyecto/spikyhair/SpikyhairApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.spikyhair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikyhairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpikyhairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/spikyhair/SpikyhairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.spikyhair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpikyhairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
